4d02387fbc642262ecd7193ba47ee0b949f7a508
LabUser <ascurtu@wgu.edu>
2023-08-07 12:56:22 -0600

    updates

---
+++ README.txt
@@ -0,0 +1,29 @@
+
+Title: Scheduling Application
+
+Purpose: The purpose of this application is a GUI-based scheduling desktop application. The application is used by a
+fictional global contracting organization to add, modify, and delete both customers and appointments. It also includes
+reports and a user-friendly GUI. The application is connected and pulls data from a MySQL database.
+
+Author/Contact: Alexandra Scurtu; Student ID: 010872975
+
+Student Application Version/Date: 1.0; 8/7/2023
+
+IDE: IntelliJ Community 2021.1.3 x 64
+
+JDK of Version Used:  Java jdk - 17.0.1
+
+JavaFX Version: jfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2
+
+Direction to Run Program: Please enter "test" or "admin" for valid username and password to login and use the application.
+
+Additional Report Description: The report pulls information from the MySQL database to show the appointment total for
+each country.
+
+MySQL Connector Driver Version Number: Java 8.0.25
+
+
+
+
+
+
--- src/controller/loginController.java
+++ src/controller/loginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import dao.appointmentDatabase;
 import dao.userDatabase;
 import helper.JDBC;
 import javafx.collections.ObservableList;
@@ -13,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.appointment;
 import model.user;
 
 import java.io.FileWriter;
@@ -21,6 +23,7 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Locale;
@@ -51,21 +54,21 @@ public class loginController implements Initializable {
     /** Initializes the login screen and sets the user's login display to either English or French based on computer language settings.
      */
     public void initialize(URL url, ResourceBundle resourceBundle) {
-            ZoneId LocalZoneId = ZoneId.of(TimeZone.getDefault().getID());
-            UserTimezone.setText(String.valueOf(LocalZoneId));
+        ZoneId LocalZoneId = ZoneId.of(TimeZone.getDefault().getID());
+        UserTimezone.setText(String.valueOf(LocalZoneId));
 
-            ResourceBundle Rb = ResourceBundle.getBundle("Language/Language", Locale.getDefault());
-            if (Locale.getDefault().getLanguage().equals("en") || Locale.getDefault().getLanguage().equals("fr")) {
-                Username_Lbl.setText(Rb.getString("Username"));
-                Pwd_Lbl.setText(Rb.getString("Password"));
-                Timezone_Lbl.setText(Rb.getString("Timezone"));
-                Login_Button.setText(Rb.getString("Login"));
-                ExitButton.setText(Rb.getString("Exit"));
-            }
+        ResourceBundle Rb = ResourceBundle.getBundle("Language/Language", Locale.getDefault());
+        if (Locale.getDefault().getLanguage().equals("en") || Locale.getDefault().getLanguage().equals("fr")) {
+            Username_Lbl.setText(Rb.getString("Username"));
+            Pwd_Lbl.setText(Rb.getString("Password"));
+            Timezone_Lbl.setText(Rb.getString("Timezone"));
+            Login_Button.setText(Rb.getString("Login"));
+            ExitButton.setText(Rb.getString("Exit"));
+        }
     }
 
     /**
-     * Allows the user to login to the application by ensuring correct username and password. Activity log is also implemented to track user login activity.
+     * Allows the user to login to the application by ensuring correct username and password. Activity log is also implemented to track user login activity and alert is set to alert the user if there is an upcoming appointment within 15 minutes.
      *
      * @param actionEvent button click
      */
@@ -73,12 +76,17 @@ public class loginController implements Initializable {
         String UserName = Username_TxtField.getText();
         String password = Pwd_TxtField.getText();
 
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime currentDateTimePlus15 = currentDateTime.plusMinutes(15);
+
         //creating a file that records all user login attempts
         String filename = "login_activity.txt";
-        FileWriter fileWriter = new FileWriter(filename,true);
+        FileWriter fileWriter = new FileWriter(filename, true);
         PrintWriter outputFile = new PrintWriter(fileWriter);
         Timestamp LoginAttemptTimestamp = Timestamp.valueOf(LocalDateTime.now());
         outputFile.print("Timestamp: " + LoginAttemptTimestamp + " ");
+
+        boolean LoginSuccess = false; // Initialize LoginSuccess flag
 
 
         ObservableList<user> userLogIn = userDatabase.getAllUsers();
@@ -86,16 +94,34 @@ public class loginController implements Initializable {
             if (UserName.equals(UserLoggingIn.getUserName()) && password.equals(UserLoggingIn.getPassword())) {
                 LoginSuccess = true;
                 break;
+            }
+        }
+        if (LoginSuccess) {
+            ObservableList<appointment> allAppointments = appointmentDatabase.getAllAppointments();
+            boolean upcomingAppointmentFound = false;
+
+            for (appointment upcomingApt : allAppointments) {
+                if (upcomingApt.getAppointmentStartDateTime().isBefore(currentDateTimePlus15) && upcomingApt.getAppointmentStartDateTime().isAfter(currentDateTime)) {
+                    upcomingAppointmentFound = true;
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setContentText("Appointment ID and time: " + (String.valueOf(upcomingApt.getAppointmentID()) +
+                            " " + upcomingApt.getAppointmentStartDateTime() + " is upcoming in the next 15 minutes."));
+                    alert.show();
+                    break;
                 }
             }
-        if (LoginSuccess) {
-            Parent root = FXMLLoader.load(getClass().getResource("/View/home.fxml"));
-            Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root, 1080, 720);
-            stage.setScene(scene);
-            stage.show();
-            outputFile.println("Timestamp: " + LoginAttemptTimestamp + "Login Attempt Successful By " + UserName);
-        } else if (!LoginSuccess) {
+            if (!upcomingAppointmentFound) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("There are no upcoming appointments in the next 15 minutes.");
+                alert.show();
+            }
+                Parent root = FXMLLoader.load(getClass().getResource("/View/home.fxml"));
+                Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root, 1080, 720);
+                stage.setScene(scene);
+                stage.show();
+                outputFile.println("Timestamp: " + LoginAttemptTimestamp + "Login Attempt Successful By " + UserName);
+        } else {
             ResourceBundle Rb = ResourceBundle.getBundle("Language/Language", Locale.getDefault());
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText(Rb.getString("ErrorContent"));
